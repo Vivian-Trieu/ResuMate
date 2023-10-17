37b35146c97fe901528e528c09dcb6750b1816e1
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-10-17 04:36:19 -0700

    update files

--- amplify/backend/function/UserHandler/src/index.py
+++ amplify/backend/function/UserHandler/src/index.py
@@ -2,8 +2,8 @@ import json
 
 def handler(event, context):
     print(event)
-    user_id = event['pathParameters']['userId']
-    user = {'userId': user_id, 'userName': "User " + user_id}
+    user_id = event['pathParameters']['userID']
+    user = {'userID': user_id, 'userName': "User " + user_id}
 
     response = {
         'statusCode': 200,
--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Amplify, { API } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+API.configure(awsExports);
 
-function App() {
+const myAPI = "api7dcb2768"
+const path = '/users'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [users, setUsers] = useState([])
+
+  //Function to fetch from our backend and update user array
+  function getUser(e) {
+    let userID = e.input
+    API.get(myAPI, path + "/" + userID)
+       .then(response => {
+         console.log(response)
+         let newUsers = [...users]
+         newUsers.push(response)
+         setUsers(newUsers)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="user id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getUser({input})}>Get User From Backend</button>
+
+      <h2 style={{visibility: users.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       users.map((thisUser, index) => {
+         return (
+        <div key={thisUser.userID}>
+          <span><b>UserID:</b> {thisUser.userID} - <b>UserName</b>: {thisUser.userName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
