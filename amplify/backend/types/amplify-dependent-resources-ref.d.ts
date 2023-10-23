824146d9e4eed8f63d698dc487bb0cb58662828c
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-10-23 01:27:13 -0700

    pulled backend files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -7,7 +7,21 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "handleUserSaveJobListing": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "newapp732e55b5": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "queryGoogleJobs": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
