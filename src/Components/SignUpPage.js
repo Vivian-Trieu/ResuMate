60a28ed0004ea0c7ce2ac24d3394fb931de7f638
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-11-14 15:39:53 -0800

    sign up page working with backend

--- src/Components/SignUpPage.js
+++ src/Components/SignUpPage.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { API } from 'aws-amplify';
 import "./SignUpPage.css";
 
 function SignUpPage(props) {
@@ -7,13 +8,31 @@ function SignUpPage(props) {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
 
     // Check if the passwords match
     if (password !== confirmPassword) {
       alert("Passwords do not match!");
       return;
+    }
+
+    try {
+      const user = {
+        username: email,
+        password: password,
+      };
+
+      // Make API request to AWS Lambda function
+      const apiResponse = await API.post('Users', '/register', {
+        body: user,
+      });
+      // Handle response data
+      console.log('API Response:', apiResponse);
+
+    } catch (error) {
+      // Handle errors from API call
+      console.error('API Error:', error);
     }
 
     // Reset the form fields
@@ -67,3 +86,23 @@ function SignUpPage(props) {
 }
 
 export default SignUpPage;
+
+
+const myAPI = "apiname"
+const path = '/pathname';
+
+function postRequestToSomeAPI(e) {
+  API.post(myAPI, path, {
+    contentType: "application/json",
+    body: {
+      "query" : "Healthcare accounting, remote",
+      "field2" : "otherstuff",
+    }
+  })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
