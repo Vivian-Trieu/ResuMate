a6064e92a82db1cdcf7f3daab9d91582f75221c9
mievenfall <mngoc2603@gmail.com>
2023-11-20 12:21:23 -0800

    added styling attributes to checkboxes

--- src/Components/Checkbox.js
+++ src/Components/Checkbox.js
@@ -3,13 +3,23 @@ import "./Checkbox.css"
 
 const Checkbox = (props) => {
   const [isChecked, setIsChecked] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const filled = {backgroundColor: '#00a7a6'}
+  const unfilled = {backgroundColor: "#e9e9e9"}
+  const checkedBoxUnfilled = {border: "0.15em solid #8e8e8e"}
+  const checkedBoxFilled = {border: "none"}
+  const textFilled = {color: "#ffffff"}
+  const textUnfilled = {color: "#8e8e8e"}
+
 
   return (
-    <div className="checkbox-wrapper">
-      <label>
-        <input className={isChecked ? "checked" : ""} type="checkbox" checked={isChecked} onChange={() => setIsChecked((prev) => !prev)}/>
-        <div className="checkbox-title">{props.label}</div>
-      </label>
+    <div className="checkbox-wrapper" style={isFilled ? filled : unfilled}>
+      {/* <div className={isFilled ? "filled" : ""}> */}
+        <label>
+          <input className={isChecked ? "checked" : ""} type="checkbox" style={isFilled ? checkedBoxFilled : checkedBoxUnfilled} checked={isChecked} onChange={() => {setIsChecked((prev) => !prev); setIsFilled((prev) => !prev)}}/>
+          <div className="checkbox-title" style={isFilled ? textFilled : textUnfilled}>{props.label}</div>
+        </label>
+      {/* </div> */}
     </div>
   );
 };
