a53905daa686e85620dbbb85598f29035ed51ccd
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-11-26 03:34:32 -0800

    try to fetch jobs from google query and display

--- src/Components/HomeScreen.js
+++ src/Components/HomeScreen.js
@@ -1,48 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import JobCard from './JobCard'
-import "./HomeScreen.css"
-
+import React, { useState, useEffect } from 'react';
+import "./HomeScreen.css";
+import JobCard from './JobCard';
+import Amplify, { API } from 'aws-amplify';
 
 function HomeScreen({ likedJobs, setLikedJobs }) {
 
-  const [jobs, setJobs] = useState([
-    {
-      id: 1,
-      title: 'Software Engineer',
-      company: 'Google',
-      location: 'Mountain View, CA',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat \
-                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
-                        deserunt mollit anim id est laborum.\
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat \
-                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
-                        deserunt mollit anim id est laborum.\
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat \
-                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
-                        deserunt mollit anim id est laborum.'
-    },
-    {
-      id: 2,
-      title: 'Product Manager',
-      company: 'Microsoft',
-      location: 'Redmond, WA',
-      description: 'def'
-    },
-    // etc
-  ]);
-
+  const [jobs, setJobs] = useState([]);
   const [currentJobIndex, setCurrentJobIndex] = useState(0);
+  const [currentScreen, setCurrentScreen] = useState('loading');
 
-  const [currentScreen, setCurrentScreen] = useState('swiping');
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const apiResponse = await API.post('Jobs', '/query', {
+          contentType: "application/json",
+          query: "{\"query\":\"New Jersey, BPO, data trend analysis, MSFT Office\"}", // Example query input from function test event.json
+        });
+
+        console.log('Resume Info API Response:', apiResponse);
+        const data = await apiResponse.json();
+        setJobs(data.matches);
+        setCurrentScreen('swiping');
+
+      } catch (error) {
+        console.log('Error fetching jobs:', error);
+        setCurrentScreen('error');
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   function handleLike() {
     const job = jobs[currentJobIndex];
@@ -66,7 +53,19 @@ function HomeScreen({ likedJobs, setLikedJobs }) {
 
   let content;
 
-  if (currentScreen === 'swiping') {
+  if (currentScreen === 'loading') {
+    content = (
+      <div className="home-screen-container">
+        <p>Loading...</p>
+      </div>
+    );
+  } else if (currentScreen === 'error') {
+    content = (
+      <div className="home-screen-container">
+        <p>Error fetching jobs. Please try again later.</p>
+      </div>
+    );
+  } else if (currentScreen === 'swiping') {
     content = (
       <>
         <div className="header-placeholder"></div>
@@ -82,18 +81,14 @@ function HomeScreen({ likedJobs, setLikedJobs }) {
     );
   } else if (currentScreen === 'no-more') {
     content = (
-      <>
-        <div className="header-placeholder"></div>
-        <div className="home-screen-container">
-          <div className="job-box">
-            <div className="job no-more">
-              <h2>No more jobs</h2>
-              <p>You've seen all available jobs. Check back later.</p>
-            </div>
+      <div className="home-screen-container">
+        <div className="job-box">
+          <div className="job no-more">
+            <h2>No more jobs</h2>
+            <p>You've seen all available jobs. Check back later.</p>
           </div>
         </div>
-        <div className="header-placeholder"></div>
-      </>
+      </div>
     );
   }
 
