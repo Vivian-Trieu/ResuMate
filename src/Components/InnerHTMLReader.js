d593bf2c3eb6edcae915f98752e2e3f73e3ecc0d
mievenfall <mngoc2603@gmail.com>
2023-12-10 11:20:40 -0800

    fix encoding error in job card

--- src/Components/InnerHTMLReader.js
+++ src/Components/InnerHTMLReader.js
@@ -2,8 +2,35 @@ import React from "react";
 import "./InnerHTMLReader.css";
 
 function InnerHTMLReader(props) {
+    // Set up mappings of incorrect -> correct symbols
+    const symbolCorrections = {
+        "\u201D": '"',
+        "\u201C": '"', 
+        "\u2018": "'",
+        "\u2019": "'",
+        "\u00B1": "+/-",
+        "\u2013": "-",
+        "â€™": "'",
+        "Â®": "®",
+        "â€\"": "—"
+      };
+  
+  // Function to fix symbols 
+  function fixSymbols(html) {
+    for (let incorrect in symbolCorrections) {
+      let correct = symbolCorrections[incorrect];
+      html = html.replace(new RegExp(incorrect, 'g'), correct); 
+    }
+    return html; 
+  }
+  
+  // Usage:
+  let html = props.value;
+  html = fixSymbols(html);
+
+
     return (
-        <div className="html" dangerouslySetInnerHTML={{__html: props.value}} />  
+        <div className="html" dangerouslySetInnerHTML={{__html: html}} />  
     );
 }
 
